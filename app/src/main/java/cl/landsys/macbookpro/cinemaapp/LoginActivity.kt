4f28463a4039c5495537bb2880cdb45b3ef9bf2e
Brian Gaete Alvear <b.gaete.digilab@gmail.com>
2018-11-04 02:17:55 -0300

    vista registro de usuario sin validaciones

--- app/src/main/java/cl/landsys/macbookpro/cinemaapp/LoginActivity.kt
+++ app/src/main/java/cl/landsys/macbookpro/cinemaapp/LoginActivity.kt
@@ -1,12 +1,22 @@
 package cl.landsys.macbookpro.cinemaapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        RegistrarTxV.setOnClickListener { registrar() }
+
+    }
+
+    fun registrar(){
+        val intent = Intent(this.applicationContext,RegisterActivity::class.java )
+        startActivity(intent)
+        finish()
     }
 }
