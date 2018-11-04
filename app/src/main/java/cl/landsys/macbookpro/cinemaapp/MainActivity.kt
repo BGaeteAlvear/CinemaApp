c6d72c980e9c1928efff52279c9e333b84fd4059
Brian Gaete Alvear <b.gaete.digilab@gmail.com>
2018-11-04 01:26:41 -0300

    vista login

--- app/src/main/java/cl/landsys/macbookpro/cinemaapp/MainActivity.kt
+++ app/src/main/java/cl/landsys/macbookpro/cinemaapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package cl.landsys.macbookpro.cinemaapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this.applicationContext,LoginActivity::class.java )
+        startActivity(intent)
+        finish()
     }
 }
